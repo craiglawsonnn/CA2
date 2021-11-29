1e4962ef57b8d36a69ed941ac63763685d78e367
craiglawsongit <S00209542@mail.itsligo.ie>
2021-11-29 14:04:38 +0000

    Initializing properties for class and laying out wpf

---
+++ CA2/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Activity
+    {
+        public string Name { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public decimal Cost { get; set; }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public enum TypeOfActivity { Air, Water, Land }
+
+    }
+}
